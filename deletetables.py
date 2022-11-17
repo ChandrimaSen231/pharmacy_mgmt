25f811f4a4fdfae9a6fbec93b9fdc7eb5ca34fe3
ChandrimaSen231 <80187873+ChandrimaSen231@users.noreply.github.com>
2022-11-17 06:57:51 +0530

    Delete deletetables.py

--- deletetables.py
+++
@@ -1,8 +0,0 @@
-import sqlite3 as sql
-
-conn = sql.connect("test.db")
-cursor = conn.cursor()
-
-cursor.execute("DROP TABLE IF EXISTS inventory_order;")
-cursor.execute("DROP TABLE IF EXISTS items;")
-conn.commit()
